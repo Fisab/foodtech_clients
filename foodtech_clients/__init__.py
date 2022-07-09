e26042dcf8afb98ff76ed9dede5a6fc61d099537
Vlad Sudakov <vladsudakov5@gmail.com>
2022-07-09 15:30:18 +0300

    поправил пути в __init__.py

--- foodtech_clients/__init__.py
+++ foodtech_clients/__init__.py
@@ -1,4 +1,4 @@
 from .__version__ import __version__
 
-from delivery_club import DeliveryClubClient
-from papa_johns import PapaJohnsClient
+from .delivery_club import DeliveryClubClient
+from .papa_johns import PapaJohnsClient
